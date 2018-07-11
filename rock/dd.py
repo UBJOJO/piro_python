bd38fc0f9dc48719d4a73de584c506a05a3bf546
UBJOJO <jjh1004ss@gmail.com>
2018-07-11 20:10:27 +0900

    2018-07-11

--- rock/dd.py
+++ rock/dd.py
@@ -1,35 +1,23 @@
-class User:
-    def __init__(self, name, email, password):
-        self.name = name
-        self.email = email
-        self.password = password
-        self.following_list = []
-        self.followers_list = []
+# Num = int(input())
+# subNumber = list(map(int, input().split()))
+# subNumber.sort()
+# H_score = subNumber[Num - 1]
+#
+# for i in range(Num):
+#     subNumber[i] = subNumber[i] / H_score * 100
+#
+# sum = 0
+# for i in range(Num):
+#     sum += subNumber[i]
+#
+# aver = sum / Num
+# print('%.2f' % aver)
+SubNum = int(input())
+subNumber = []
+subAv = []
+for i in range(SubNum):
+    subNumber[i] = list(map(int, input().split()))
+    subAv[i] = subNumber[i]/subNumber[i][0]
+    print(subNumber[i])
+    print(subAv[i])
 
-    def follow(self, another_user):
-        self.following_list.append(another_user)
-        another_user.followers_list.append(self)
-
-    def num_following(self):
-        return len(self.following_list)
-
-    def num_followers(self):
-        return len(self.followers_list)
-
-
-user1 = User("Young", "young@codeit.kr", "123456")
-user2 = User("Yoonsoo", "yoonsoo@codeit.kr", "abcdef")
-user3 = User("Taeho", "taeho@codeit.kr", "123abc")
-user4 = User("Lisa", "lisa@codeit.kr", "abc123")
-
-user1.follow(user2)
-user1.follow(user3)
-user2.follow(user1)
-user2.follow(user3)
-user2.follow(user4)
-user4.follow(user1)
-
-print(user1.name, user1.num_followers(), user1.num_following())
-print(user2.name, user2.num_followers(), user2.num_following())
-print(user3.name, user3.num_followers(), user3.num_following())
-print(user4.name, user4.num_followers(), user4.num_following())
